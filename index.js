fe6cb912ff6c70df846849c751e34a7bb223c4c0
Mikolaj <dlugaibezsensownanazwa@gmail.com>
2025-10-08 07:26:33 +0000

    02

--- index.js
+++ index.js
@@ -12,30 +12,13 @@ const server = http.createServer(function (req, res) {
     res.setHeader("Content-Type", "text/html");
     res.end(index_html);
   }
-
   if (req.url && req.url === "/favicon.ico") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "image/vnd.microsoft.icon");
     res.end(favicon_ico);
   }
-
-  if (req.url && req.url === "/json") {
-    const content = {
-      message: "Hello there",
-      time: Intl.DateTimeFormat("pl", {
-        dateStyle: "full",
-        timeStyle: "long",
-        timeZone: "CET",
-      }).format(Date.now()),
-    };
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(content));
-  }
-
   res.statusCode = 404;
   res.end();
 });
-
 server.listen(port);
 console.log(`Server listening on port http://localhost:${port}`);
