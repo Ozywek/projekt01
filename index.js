26baf49ab33c6fadf802fb22b6764e657d46c922
Ozywek <138172500+Ozywek@users.noreply.github.com>
2025-10-08 09:34:01 +0200

    03

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ const index_html = fs.readFileSync("index.html");
 const favicon_ico = fs.readFileSync("favicon.ico");
 
 const server = http.createServer(function (req, res) {
-  if (req.url && req.url === "/") {
+  if (req.method !== "GET") {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Method not allowed\n");
+  }
+  else if (req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.end(index_html);
   }
-  if (req.url && req.url === "/favicon.ico") {
+  else if (req.url === "/favicon.ico") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "image/vnd.microsoft.icon");
     res.end(favicon_ico);
+  }
+  else{
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Site not found!\n");
   }
   res.statusCode = 404;
   res.end();
